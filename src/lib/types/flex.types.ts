7761cb07024d6423072f351565cde954466bfb64
Dhaval Patel <pateldhaval3890@gmail.com>
2023-09-02 09:55:07 +0530

    stack component updated

---
+++ src/lib/types/flex.types.ts
@@ -0,0 +1,3 @@
+export type FlexDirection = 'column' | 'column-reverse' | 'row' | 'row-reverse';
+export type JustifyContent = 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
+export type AlignItems = 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'baseline';
--- src/lib/ui/layers/Stack.tsx
+++ src/lib/ui/layers/Stack.tsx
@@ -1,13 +1,18 @@
+import { AlignItems, FlexDirection, JustifyContent } from '@/lib/types/flex.types';
 import { spacing } from '@/lib/utils/tokens.utils';
 import styled from '@emotion/styled';
 
 interface IProps {
-	direction?: 'column' | 'row';
+	direction?: FlexDirection;
+	justifyContent?: JustifyContent;
+	alignItems?: AlignItems;
 	gap?: number;
 }
 
 export const Stack = styled.div<IProps>`
 	display: flex;
 	flex-direction: ${(props) => props.direction ?? 'column'};
+	justify-content: ${(props) => props.justifyContent ?? 'flex-start'};
+	align-items: ${(props) => props.alignItems ?? 'stretch'};
 	gap: ${(props) => spacing[props.gap ?? 0]};
 `;
