3711fdbdc8ac7bf4b3ca366936aa3f93955b94cb
Dhaval Patel <pateldhaval3890@gmail.com>
2023-11-01 14:13:00 +0530

    fetch to axios convert

--- src/lib/hooks/useGetRequest.ts
+++ src/lib/hooks/useGetRequest.ts
@@ -1,12 +1,13 @@
+import axios from 'axios';
+
 import { useQuery } from '@tanstack/react-query';
 
 export const useGetRequest = <T>(key: string, url: string) => {
 	const { isLoading, data, error } = useQuery<T>({
 		queryKey: [key],
 		queryFn: async () => {
-			const res = await fetch(url);
-			const data = await res.json();
-			return data;
+			const res = await axios.get<T>(url);
+			return res.data;
 		}
 	});
 
